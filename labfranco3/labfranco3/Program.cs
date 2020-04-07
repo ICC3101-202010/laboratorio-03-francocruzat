785225fdcbbceda473691216129aa72d8ee105fc
Franco Cruzat <fhcruzat@miuandes.cl>
2020-04-07 16:35:32 -0300

    last commit

--- labfranco3/labfranco3/Program.cs
+++ labfranco3/labfranco3/Program.cs
@@ -5,12 +5,12 @@ namespace labfranco3
 {
     class Persona
     {
-        private int Rut;
-        private string Nombre;
-        private string Apellido;
-        private string Fecha;
-        private string Nacionalidad;
-        private string Estadocivil;
+        public int Rut;
+        public string Nombre;
+        public string Apellido;
+        public string Fecha;
+        public string Nacionalidad;
+        public string Estadocivil;
 
         public Persona(int rut,string nombre, string apellido,string fecha, string nacionalidad,string estadocivil)
         {
@@ -25,8 +25,8 @@ namespace labfranco3
     }
     class Cliente : Persona
     {
-        private int Dinero;
-        private string Domicilio;
+        public int Dinero;
+        public string Domicilio;
 
         public Cliente(int rut, string nombre, string apellido, string fecha, string nacionalidad,string estadocivil,int dinero,string domicilio)
             : base(rut, nombre, apellido, fecha, nacionalidad, estadocivil)
@@ -39,12 +39,12 @@ namespace labfranco3
     }
     class Trabajador : Persona
     {
-        private int Sueldo;
-        private string Trabajo;
-        private string Horario;
+        public int Sueldo;
+        public string Trabajo;
+        public string Horario;
 
-        public Trabajador(int rut, string nombre, string apellido, string fecha, string nacionalidad, int sueldo, string trabajo,string horario)
-            : base(rut, nombre, apellido, fecha, nacionalidad)
+        public Trabajador(int rut, string nombre, string apellido, string fecha, string nacionalidad, string estadocivil, int sueldo,string trabajo,string horario)
+            : base(rut, nombre, apellido, fecha, nacionalidad, estadocivil)
         {
             this.Sueldo = sueldo;
             this.Trabajo = trabajo;
@@ -66,10 +66,10 @@ namespace labfranco3
 
     class Producto
     {
-        private int Precio;
-        private string Nombreproducto;
-        private string Marca;
-        private int Stockdisponible;
+        public int Precio;
+        public string Nombreproducto;
+        public string Marca;
+        public int Stockdisponible;
 
 
 
@@ -93,10 +93,18 @@ namespace labfranco3
         }
         public void Comprar(Cliente cliente,Trabajador cajero,List<string> listproducto)
         {
+            for (int i = 0; i < listproducto.Count; ++i)
+            {
+                for (int j = 0; j < Listaproductosdisponible.Count; ++j)
+                {
+                    if (listproducto[i] == Listaproductosdisponible[j] && Listaproductosdisponible[j].Stockdisponiblez>0)
+                    {
+                        listproducto[i].Stockdisponible-=1 //falta codigo para los registros
+                    }
+                }
+            }
         }
-
     }
-    
     class Registro
     {
         private string Fecharegistro;
@@ -112,17 +120,63 @@ namespace labfranco3
             this.Cliente = cliente;
             this.Cajero = cajero;
             this.Listaproductoscliente = listaproductoscliente;
-
-
         }
     }
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Trabajador t1 = new Trabajador(4124, "juan", "diaz", "120922", "argentina", 100000, "cajero", "20pm");
-            Console.WriteLine(t1.saludar());
 
+            int rutcliente;
+            string nombrecliente;
+            string apellidocliente;
+            string fechacliente;
+            string nacionalidadcliente;
+            string estadocivilcliente;
+            int dinerocliente;
+            string domiciliocliente;
+
+            Console.WriteLine("Ingrese los datos del cliente");
+            rutcliente = int.Parse(Console.ReadLine());
+            nombrecliente= Console.ReadLine();
+            apellidocliente= Console.ReadLine();
+            fechacliente= Console.ReadLine();
+            nacionalidadcliente= Console.ReadLine();
+            estadocivilcliente= Console.ReadLine();
+            dinerocliente= int.Parse(Console.ReadLine());
+            domiciliocliente= Console.ReadLine();
+            Cliente cliente = new Cliente(rutcliente, nombrecliente, apellidocliente, fechacliente, nacionalidadcliente, estadocivilcliente, dinerocliente, domiciliocliente);
+            int ruttrabajador;
+            string nombretrabajador;
+            string apellidotrabajador;
+            string fechatrabajador;
+            string nacionalidadtrabajador;
+            string estadociviltrabajador;
+            int sueldotrabajador;
+            string trabajotrabajador;
+            string horariotrabajador
+
+            Console.WriteLine("Ingrese los datos del trabajador");
+
+            ruttrabajador = int.Parse(Console.ReadLine());
+            nombretrabajador = Console.ReadLine();
+            apellidotrabajador = Console.ReadLine();
+            fechatrabajador = Console.ReadLine();
+            nacionalidadtrabajador = Console.ReadLine();
+            estadociviltrabajador = Console.ReadLine();
+            sueldotrabajador = int.Parse(Console.ReadLine());
+            trabajotrabajador = Console.ReadLine();
+            horariotrabajador= Console.ReadLine();
+            Trabajador trabajador = new Trabajador(ruttrabajador, nombretrabajador, apellidotrabajador, fechatrabajador, nacionalidadtrabajador, estadociviltrabajador, sueldotrabajador, trabajotrabajador,horariotrabajador);
+
+            Console.WriteLine("Ingrese nuevo sueldo, nuevo trabajador, nuevo horario en respectivo orden");
+
+            sueldonuevo = int.Parse(Console.ReadLine());
+            trabajonuevo = Console.ReadLine();
+            horarionuevo = Console.ReadLine();
+            trabajador.Cambiarsueldo(sueldonuevo);
+            trabajador.Cambiartrabajo(trabajonuevo);//creo que hice algo mal aqui, no alcanze a arreglarlo ;(;(;(
+            trabajador.CambiarHorario(horarionuevo);// falta terminar pocas cosas en el programa que se hacer, pero no alcanze(irresponsabilidad);((((
         }
     }
 }
